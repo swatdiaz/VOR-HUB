34c3165c3692f62bd09912f4f955df6f9e425e55
swatdiaz <95551899+swatdiaz@users.noreply.github.com>
2026-07-30 03:55:20 -0400

    Use Submarine Worker interaction distance - pin loader

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 -- VOR Hub immutable modular loader.
 -- Release tooling replaces the placeholder with the audited module commit.
 
-local COMMIT = "64e3ce25e6e0b6daf6f1b9c0c5b5574dd9844b48"
+local COMMIT = "5e1572548b4939f7e8e4f6f9c300754a7704f080"
 local REPOSITORY = "swatdiaz/VOR-HUB"
 
 if not game:IsLoaded() then
