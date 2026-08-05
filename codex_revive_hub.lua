ee3a425aba4f9d7347cabdaf4150014457e2787e
swatdiaz <95551899+swatdiaz@users.noreply.github.com>
2026-08-05 05:30:46 -0400

    Remove retired Revive standalone loader

--- codex_revive_hub.lua
+++
@@ -1,2 +0,0 @@
--- Legacy filename compatibility. It resolves the same immutable audited loader.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/swatdiaz/VOR-HUB/1a962602848e43b08723f547c06388f50426d814/loader.lua"))()
